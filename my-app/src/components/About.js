ce7e3d8be69bd89008651e517c6bbf4ffed12a81
roselandroche <r.landroche@gmail.com>
2020-07-21 20:35:14 -0400

    Research/note taking into About page

---
+++ my-app/src/components/About.js
@@ -0,0 +1,21 @@
+// About
+
+/*
+What is it?
+It is a cellular automaton developed by John Conway in 1970. It is a 0 player game - aside from the 
+    initial configuration no user input is required. The state changes and evolves on its own.
+It is composed of a grid of cells that follow rules from which complex behaviors can emerge.
+
+Why is it useful?
+This can be used in simulations for biological or chemical or other scenarios, to assist in research.
+
+How is Turing Completeness related?
+A computing system is Turing Complete if it is capable of performing arbitrary, general purpose computation.
+    Conway's Game of Life is a computing system that qualifies.
+
+What are the rules?
+    Birth?
+        If the current cell is dead, but 3 neighboring cells are alive, it comes to life.
+    Death?
+        If the current cell is alive, it needs 2 to 3 living neighboring cells to survive, else it dies.
+*/
