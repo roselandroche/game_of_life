257285612cd2a42f43ec988ea380146b93e08239
roselandroche <r.landroche@gmail.com>
2020-07-22 21:54:03 -0400

    Created a 2D array

--- my-app/src/components/Grid.js
+++ my-app/src/components/Grid.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+const numRows = 25;
+const numCol = 25;
 
 function Grid() {
+    const [grid, setGrid] = useState(() => {
+        const rows = []
+        for(let i = 0; i < numRows; i++) {
+            rows[i] = Array(numCol - 1).fill(0);
+        }
+        return rows;
+    })
+    console.log(grid)
     return (
         <div>
             <h1>This will be a GRID</h1>
