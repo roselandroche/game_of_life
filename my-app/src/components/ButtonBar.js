700e6d50c75bc5a26f617f85dbf9d3e0fce663bf
roselandroche <r.landroche@gmail.com>
2020-07-23 21:36:03 -0400

    Understanding and starting to build functionality of game

--- my-app/src/components/ButtonBar.js
+++ my-app/src/components/ButtonBar.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/ButtonBar.css';
 
 function ButtonBar() {
+    const [running, setRunning] = useState(false)
+
+    const startProgram = () => {
+        if(!running) {
+            setRunning(true)
+        }
+        console.log(running)
+    }
+
+    const stopProgram = () => {
+        if(running) {
+            setRunning(false)
+        }
+        console.log(running)
+    }
+
+    const clearGrid = () => {
+        console.log(`Clearing the grid...`)
+    }
+
     return (
         <div className='bar'>
-            <div className='start'>
+            <button className='start' onClick={() => {startProgram()}}>
                 Start
-            </div>
-            <div className='stop'>
+            </button>
+            <button className='stop' onClick={() => {stopProgram()}}>
                 Stop
-            </div>
-            <div className='clear'>
+            </button>
+            <button className='clear' onClick={() => {clearGrid()}}>
                 Clear
-            </div>
+            </button>
         </div>
     )
 }
